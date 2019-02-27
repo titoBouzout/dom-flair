028cc9c17b75a91b93d279d54dd4f64fc90c5f7f
Tito Bouzout <tito.bouzout@gmail.com>
2019-02-27 08:54:56 -0300

    Add npm stuff

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express()
 
 var server = require('http').createServer(app)
 
+var serveIndex = require('serve-index')
+
 app.use(express.static('./www/'))
+app.use(serveIndex('./www/'))
 
 server.listen(80)
